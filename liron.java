a1b715311aef1ce4f5994ed2719d7f7ef794b704
lironoshka111 <46004238+lironoshka111@users.noreply.github.com>
2024-05-11 16:21:23 +0300

    Update liron.java

--- liron.java
+++ liron.java
@@ -2,7 +2,7 @@
 public class liron {
     public static void main(String[] args) {
         for (int i = 0; i < 5; i++) {
-            System.out.println("liron");
+            System.out.println("lili");
         }
     }
 }
