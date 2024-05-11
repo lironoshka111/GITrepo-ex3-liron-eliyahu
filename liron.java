0d866ca2814e85b68a87a681e1b239066ccb6bff
liron <lironoshka111@gmail.com>
2024-05-11 14:46:26 +0300

    first commit

---
+++ liron.java
@@ -0,0 +1,8 @@
+// liron.java
+public class liron {
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; i++) {
+            System.out.println("liron");
+        }
+    }
+}
